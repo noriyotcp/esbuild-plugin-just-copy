3fd3a9b3a0b35f0d0a1c4dcd225933eeb9c48bc4
noriyotcp <noriyo.akita@gmail.com>
2022-11-10 13:28:03 +0900

    Preparation
    assets is Array of objects
    
    {
      from: [],
      to: [],
      keepStructure: boolean,
    }
    
    push error when `from` or `to` path ends with '/'
    
    workaround

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { PluginBuild, BuildResult } from "esbuild/lib/main.d";
+import { PluginBuild, BuildResult, OnResolveResult } from "esbuild/lib/main.d";
 
 export const hello = (message: string): string => {
   const str = `message is ${message}`;
@@ -6,13 +6,43 @@ export const hello = (message: string): string => {
   return str;
 };
 
-export const justCopy = () => {
+export interface Options {
+  resolveFrom: "cwd" | string;
+  assets: Array<{
+    [key: number]: string;
+    from: Array<string>;
+    to: Array<string>;
+    keepStructure: boolean;
+  }>;
+}
+
+export const justCopy = (options: Options) => {
+  const {
+    resolveFrom = "cwd",
+    assets = {
+      keepStructure: false,
+    },
+  } = options;
+
+  const errors: { text: string; }[] = [];
+  const from = options.assets[0].from[0];
+  const to = options.assets[0].to[0];
+
+  console.log(options);
+  console.log(from, to);
+  console.log(resolveFrom, assets); // workarounds for unused variables
+
   return {
     name: "just-copy",
     setup(build: PluginBuild) {
-      build.onStart(async (): Promise<null> => {
+      build.onStart(async (): Promise<OnResolveResult> => {
         hello("This is just-copy");
-        return null;
+        if (/\/$/.test(from)) {
+          errors.push({ text: "`from` must not end with `/`" });
+        } else if (/\/$/.test(to)) {
+          errors.push({ text: "`to` must not end with `/`" });
+        }
+        return { errors };
       });
 
       build.onEnd((result: BuildResult) => {
