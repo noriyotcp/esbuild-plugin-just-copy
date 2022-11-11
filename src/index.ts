026d22dbd4988491e7121bef447775c7d7c8d011
noriyotcp <noriyo.akita@gmail.com>
2022-11-11 09:48:57 +0900

    Iterate over options.assets

--- src/index.ts
+++ src/index.ts
@@ -21,55 +21,47 @@ const isGlob = (_path: string) => {
 };
 
 export const justCopy = (options: Options) => {
-  const {
-    resolveFrom = "cwd",
-    assets = {
-      keepStructure: false,
-    },
-  } = options;
-
   const errors: { text: string }[] = [];
-  const from = options.assets[0].from[0];
-  const to = options.assets[0].to[0];
 
   console.log(options);
-  console.log(from, to);
-  console.log(resolveFrom, assets); // workarounds for unused variables
 
   return {
     name: "just-copy",
     setup(build: PluginBuild) {
       build.onStart(async (): Promise<OnResolveResult> => {
-        if (/\/$/.test(from)) {
-          errors.push({ text: "`from` must not end with `/`" });
-        } else if (/\/$/.test(to)) {
-          errors.push({ text: "`to` must not end with `/`" });
-        }
+        for (const asset of options.assets) {
+          const [from, to] = [asset.from[0], asset.to[0]];
 
-        // if the path is globbed
-        if (isGlob(from)) {
-          CopyFilesRecursively(from.replace(`/**/*`, ""), to);
-        } else {
-          // copy a single file
-          try {
-            if (!fs.statSync(from).isFile()) {
-              errors.push({ text: `${from} is not a file` });
-              return { errors };
-            }
-          } catch (err) {
-            if (err instanceof Error) {
-              errors.push({ text: err.message });
-              return { errors };
-            }
+          if (/\/$/.test(from)) {
+            errors.push({ text: "`from` must not end with `/`" });
+          } else if (/\/$/.test(to)) {
+            errors.push({ text: "`to` must not end with `/`" });
           }
+          // if the path is globbed
+          if (isGlob(from)) {
+            CopyFilesRecursively(from.replace(`/**/*`, ""), to);
+          } else {
+            // copy a single file
+            try {
+              if (!fs.statSync(from).isFile()) {
+                errors.push({ text: `${from} is not a file` });
+                return { errors };
+              }
+            } catch (err) {
+              if (err instanceof Error) {
+                errors.push({ text: err.message });
+                return { errors };
+              }
+            }
 
-          try {
-            fs.mkdirSync(path.dirname(to), { recursive: true });
-            fs.copyFileSync(from, to);
-          } catch (err) {
-            if (err instanceof Error) {
-              errors.push({ text: err.message });
-              return { errors };
+            try {
+              fs.mkdirSync(path.dirname(to), { recursive: true });
+              fs.copyFileSync(from, to);
+            } catch (err) {
+              if (err instanceof Error) {
+                errors.push({ text: err.message });
+                return { errors };
+              }
             }
           }
         }
