40c1829a71c33d210dfd10c58addddbe7e9ae3cd
noriyotcp <noriyo.akita@gmail.com>
2022-11-11 11:44:08 +0900

    Move getFilelistRecursively() to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import { PluginBuild, BuildResult, OnResolveResult } from "esbuild/lib/main.d";
-import { getFilelistRecursively } from "./lib/getFilelistRecursively.mjs";
 
 import fs from "node:fs";
 import path from "node:path";
@@ -18,6 +17,25 @@ const isGlob = (_path: string) => {
   const { dir } = path.parse(_path);
 
   return dir.endsWith("/**");
+};
+
+/**
+ * serach files and directories recursively.
+ * @param {string} targetpath search base path
+ * @returns list of { path:absolute path, isDir:is directory } of files and dirs in targetpath
+ */
+const getFilelistRecursively = (targetpath: fs.PathLike) => {
+  let result: { path: string; isDir: boolean }[] = [];
+  const dirs = fs.readdirSync(targetpath);
+  dirs.forEach((file) => {
+    const filepath = `${targetpath}/${file}`;
+    const isDir = fs.lstatSync(filepath).isDirectory();
+    result.push({ path: filepath, isDir: isDir });
+    if (isDir) {
+      result = result.concat(getFilelistRecursively(filepath));
+    }
+  });
+  return result;
 };
 
 export const justCopy = (options: Options) => {
