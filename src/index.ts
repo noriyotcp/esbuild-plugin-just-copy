d7e426d071606576f45f4a701df9c9a985d7fe32
noriyotcp <noriyo.akita@gmail.com>
2022-11-11 12:00:40 +0900

    Improve try~catch

--- src/index.ts
+++ src/index.ts
@@ -47,58 +47,50 @@ export const justCopy = (options: Options) => {
     name: "just-copy",
     setup(build: PluginBuild) {
       build.onStart(async (): Promise<OnResolveResult> => {
-        for (const asset of options.assets) {
-          const [from, to] = [asset.from[0], asset.to[0]];
+        try {
+          for (const asset of options.assets) {
+            const [from, to] = [asset.from[0], asset.to[0]];
 
-          if (/\/$/.test(from)) {
-            errors.push({ text: "`from` must not end with `/`" });
-          } else if (/\/$/.test(to)) {
-            errors.push({ text: "`to` must not end with `/`" });
-          }
-          // if the path is globbed
-          if (isGlob(from)) {
-            const fromDir = from.replace(`/**/*`, "");
-
-            if (!fs.existsSync(to)) {
-              fs.mkdirSync(to, { recursive: true });
+            if (/\/$/.test(from)) {
+              errors.push({ text: "`from` must not end with `/`" });
+            } else if (/\/$/.test(to)) {
+              errors.push({ text: "`to` must not end with `/`" });
             }
-            const targetList = getFilelistRecursively(fromDir);
-            targetList.forEach((node) => {
-              const newpath = to + node.path.substring(fromDir.length);
-              if (node.isDir) {
-                if (!fs.existsSync(to)) fs.mkdirSync(newpath);
-              } else {
-                fs.copyFile(node.path, newpath, (err) => {
-                  if (err) throw err;
-                });
+            // if the path is globbed
+            if (isGlob(from)) {
+              const fromDir = from.replace(`/**/*`, "");
+
+              if (!fs.existsSync(to)) {
+                fs.mkdirSync(to, { recursive: true });
               }
-            });
-          } else {
-            // copy a single file
-            try {
+
+              const targetList = getFilelistRecursively(fromDir);
+              targetList.forEach((node) => {
+                const newpath = to + node.path.substring(fromDir.length);
+
+                if (node.isDir) {
+                  if (!fs.existsSync(to)) fs.mkdirSync(newpath);
+                } else {
+                  fs.copyFileSync(node.path, newpath);
+                }
+              });
+            } else {
+              // copy a single file
               if (!fs.statSync(from).isFile()) {
                 errors.push({ text: `${from} is not a file` });
-                return { errors };
               }
-            } catch (err) {
-              if (err instanceof Error) {
-                errors.push({ text: err.message });
-                return { errors };
-              }
-            }
-
-            try {
               fs.mkdirSync(path.dirname(to), { recursive: true });
               fs.copyFileSync(from, to);
-            } catch (err) {
-              if (err instanceof Error) {
-                errors.push({ text: err.message });
-                return { errors };
-              }
             }
           }
+        } catch (err) {
+          if (err instanceof Error) {
+            errors.push({ text: err.message });
+          }
+        } finally {
+          // eslint-disable-next-line no-unsafe-finally
+          return { errors };
         }
-        return { errors };
       });
 
       build.onEnd((result: BuildResult) => {
